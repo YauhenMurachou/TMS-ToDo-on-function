4ef1dc06d340eb517eed0ca80d3d373b56df3ecd
YauhenMurachou <emurach1992@gmail.com>
2021-08-26 00:32:10 +0300

    filter by search and block of identify items

--- src/components/ToDoApp/ToDoApp.js
+++ src/components/ToDoApp/ToDoApp.js
@@ -1,19 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ToDoList from '../ToDoList/ToDoList';
 import './ToDoApp.css'
 
 function ToDoApp() {
 
 	const [items, setItems] = useState([]);
+	const [searchItems, setSearchItems] = useState(null);
 	const [text, setText] = useState('');
+	const [searchText, setSearchText] = useState('');
+
+	const inputAddEl = useRef(null);
+	const inputSearchEl = useRef(null);
 
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/todos')
 			.then((response) => response.json())
-			.then((result) =>  setItems(result.slice(0, 10)))
+			.then((result) => setItems(result.slice(0, 10)))
 			.then((json) => console.log(json))
 	}, []
 	)
+
+	const handleSubmitSearch = (e) => {
+		e.preventDefault();
+		if (searchText === '') {
+			setSearchItems([])
+			console.log('setSearchItems');
+		} else {
+			const itemsCopy = [...items];
+			const searchItemsCopy = itemsCopy.filter(item => item.title.includes(searchText));
+			setSearchItems(searchItemsCopy);
+			console.log('handleSubmitSearch');
+		}
+	}
+
+	const handleChangeSearch = (e) => {
+		setSearchText(e.target.value);
+		console.log('handleChangeSearch');
+	}
 
 	const handleChange = (e) => {
 		setText(e.target.value);
@@ -29,29 +52,58 @@ function ToDoApp() {
 	const handleCheckbox = (id) => {
 		let newItems = items.slice();
 		const delId = newItems.findIndex((n) => n.id === id);
-		newItems[delId].checked = !newItems[delId].checked;
+		newItems[delId].completed = !newItems[delId].completed;
 		setItems(newItems);
 	}
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		if (text.length === 0) {
-			return;
+		let copyText = text.replace(/\s/g, '');
+		let copyItems = items.slice();
+		if (text.length === 0 || copyItems.some(item => item.title.replace(/\s/g, '') === copyText)
+			|| (searchItems && searchItems.length !== 0)) {
+			console.log(copyItems)
+			return
+		} else {
+			const newItem = {
+				title: text,
+				id: Date.now(),
+				completed: false
+			};
+			setItems(items.concat(newItem));
+			setText('');
 		}
-		const newItem = {
-			text: text,
-			id: Date.now()
-		};
-		setItems(items.concat(newItem));
-		setText('');
 	}
 
-	const changeTask = (arr) => {
+
+	const changeTask = () => {
+		let renderItems = [];
+		if (searchItems === null) {
+			renderItems = items;
+			return renderTasks(renderItems);
+		}
+		if (searchItems && searchItems.length > 0) {
+
+			renderItems = searchItems;
+			return renderTasks(renderItems);
+
+		} else if (searchItems && searchItems.length === 0 && searchText === '') {
+
+			renderItems = items;
+			return renderTasks(renderItems);
+
+		} else if (searchItems && searchItems.length === 0 && searchText !== '') {
+
+			return <span> Tasks not found </span>;
+		}
+	}
+
+
+	const renderTasks = (arr) => {
 		return (
 			<ul>
 				{arr.map((item) => (
 					<ToDoList
-						// list = {this.componentDidMount}
 						key={item.id}
 						item={item}
 						onClick={() => removeTask(item.id)}
@@ -65,13 +117,32 @@ function ToDoApp() {
 	return (
 		<div>
 			<h3>Список дел</h3>
-			{changeTask(items)}
+			<form onSubmit={handleSubmitSearch}>
+				<label className='label-ToDo' htmlFor='find-todo'>
+					Find task
+				</label>
+				<input
+					id='find-todo'
+					className='addSearch-input'
+					ref={inputSearchEl}
+					onChange={handleChangeSearch}
+					value={searchText}
+				/>
+				<br />
+				<button className='addSearch-btn'>
+					Поиск
+				</button>
+			</form>
+
+			{changeTask()}
+
 			<form onSubmit={handleSubmit}>
 				<label className='label-ToDo' htmlFor='new-todo'>
 					Что нужно сделать?
 				</label>
 				<input
 					id='new-todo'
+					ref={inputAddEl}
 					onChange={handleChange}
 					value={text}
 				/>
